9609a53a045381ac3a330c01ddcf16f3bfb21fc3
Don Clark <123608546+DonClark93@users.noreply.github.com>
2023-12-17 20:58:10 -0500

    day3

--- day3/aocd3p2.py
+++ day3/aocd3p2.py
@@ -11,50 +11,96 @@ def confirm_part(space_around_num):
     else:
         return False
 
+
 with open("doc.txt", encoding='utf-8') as doc:
     schematic = doc.readlines()
     engine = [x.strip() for x in schematic]
     total = 0
     row = 0
+    gear_ratio_total = 0
     len_of_schem = len(engine)
     ast_locations = []# list of (x,y) coords
+    gear_groups = []
+
+
+    def check_top(x,y):
+        tot_num = []
+        if engine[y-1][x-1].isnumeric() and engine[y-1][x].isnumeric() and engine[y-1][x+1].isnumeric():
+            tot_num.append(engine[y-1][x-1:x+2])
+        if engine[y-1][x-1].isnumeric() and engine[y-1][x].isnumeric() and not engine[y-1][x+1].isnumeric():
+            tot_num.append(engine[y-1][x-2:x+1].replace('.',''))
+        if engine[y-1][x-1].isnumeric() and not engine[y-1][x].isnumeric() and not engine[y-1][x+1].isnumeric():
+            tot_num.append(engine[y-1][x-3:x].replace('.',''))
+        if  not engine[y-1][x-1].isnumeric() and engine[y-1][x].isnumeric() and engine[y-1][x+1].isnumeric():
+            tot_num.append(engine[y-1][x:x+3].replace('.',''))
+        if  not engine[y-1][x-1].isnumeric() and not engine[y-1][x].isnumeric() and engine[y-1][x+1].isnumeric():
+            tot_num.append(engine[y-1][x+1:x+4].replace('.',''))
+        if  not engine[y-1][x-1].isnumeric() and engine[y-1][x].isnumeric() and not engine[y-1][x+1].isnumeric():
+            tot_num.append(engine[y-1][x].replace('.',''))
+        if  engine[y-1][x-1].isnumeric() and not engine[y-1][x].isnumeric() and engine[y-1][x+1].isnumeric():
+            tot_num.append(engine[y-1][x+1:x+4].replace('.',''))
+            tot_num.append(engine[y-1][x-3:x].replace('.',''))
+        return tot_num
+
+    def check_mid(x,y):
+        tot_num = []
+        if engine[y][x-1].isnumeric():
+            tot_num.append(engine[y][x-3:x].replace('.',''))
+        if engine[y][x+1].isnumeric():
+            tot_num.append(engine[y][x+1:x+4].replace('.',''))
+        print(tot_num)
+        return tot_num
+
+    def check_bot(x,y):
+        tot_num = []
+        if engine[y+1][x-1].isnumeric() and engine[y+1][x].isnumeric() and engine[y+1][x+1].isnumeric():
+            tot_num.append(engine[y+1][x-1:x+2])
+        if engine[y+1][x-1].isnumeric() and engine[y+1][x].isnumeric() and not engine[y+1][x+1].isnumeric():
+            tot_num.append(engine[y+1][x-2:x+1].replace('.',''))
+        if engine[y+1][x-1].isnumeric() and not engine[y+1][x].isnumeric() and not engine[y+1][x+1].isnumeric():
+            tot_num.append(engine[y+1][x-3:x].replace('.',''))
+        if  not engine[y+1][x-1].isnumeric() and engine[y+1][x].isnumeric() and engine[y+1][x+1].isnumeric():
+            tot_num.append(engine[y+1][x:x+3].replace('.',''))
+        if  not engine[y+1][x-1].isnumeric() and not engine[y+1][x].isnumeric() and engine[y+1][x+1].isnumeric():
+            tot_num.append(engine[y+1][x+1:x+4].replace('.',''))
+        if  not engine[y+1][x-1].isnumeric() and engine[y+1][x].isnumeric() and not engine[y+1][x+1].isnumeric():
+            tot_num.append(engine[y+1][x].replace('.',''))
+        if  engine[y+1][x-1].isnumeric() and not engine[y+1][x].isnumeric() and engine[y+1][x+1].isnumeric():
+            tot_num.append(engine[y+1][x+1:x+4].replace('.',''))
+            tot_num.append(engine[y+1][x-3:x].replace('.',''))
+        return tot_num
+
+    def check_gear(x,y):
+        gears =[]
+        gears += check_top(x,y)
+        gears += check_mid(x,y)
+        gears += check_bot(x,y)
+        print(gears)
+        return gears
 
     while row < len_of_schem:
-        
         for col,char in enumerate(engine[row]):
             if char == '*':
                 ast_locations.append((col,row))
         row += 1
 
     for coords in ast_locations:
-        x,y = coords
-        pass
-        
-    print("total: " + str(ast_locations))
+        x,y= coords
+        gear_groups.append(check_gear(x,y))
+
+    
+
+    for gears in gear_groups:
+        temp_tot = 0
+        if len(gears) == 1:
+            print(gears)
+        if len(gears) != 1:
+            temp_tot = int(gears[0]) * int(gears[1])
+        gear_ratio_total = gear_ratio_total + temp_tot
+
+    print(len(gear_groups))
+    print(gear_ratio_total)
+    #print("total: " + str(ast_locations))
+
 
 doc.closed
-                                  
-"""
-        for number in row_numbers:
-            test_num, start, end = number
-            space_around_num = []
-            engine_part = False
-            if start == 0:
-                start = 1
-            if end == len(engine[row]):
-                end = len(engine[row])-1
-            if row == 0:
-                space_around_num.append(engine[row][start-1:end+2])
-                space_around_num.append(engine[row+1][start-1:end+2])   
-            elif row == (len_of_schem-1):
-                space_around_num.append(engine[row-1][start-1:end+2])
-                space_around_num.append(engine[row][start-1:end+2])   
-            else:
-                space_around_num.append(engine[row-1][start-1:end+2])
-                space_around_num.append(engine[row][start-1:end+2])
-                space_around_num.append(engine[row+1][start-1:end+2])
-                
-            if confirm_part(space_around_num):
-                #print(test_num)
-                total = total + int(test_num)
-              """
